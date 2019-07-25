2afccafe2867339271d812c176d7b98af9452808
al <albertincx@gmail.com>
2019-07-25 23:56:06 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const cors = require('cors');
 const bot = require('./config/bot');
 const port = process.env.PORT || 5000;
 app.use(cors());
+app.get('/', (req, res) => res.json({ code: 200 }));
 app.use('/bot', botroute(bot));
 app.listen(port, () => console.info(`server started on port ${port}`));
 module.exports = app;
