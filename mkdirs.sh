03560986f39180192981a3f729bbcf300c1c4a5b
al <albertincx@gmail.com>
2019-08-04 16:48:53 +0300

    fix docker compose
    (cherry picked from commit 378cc2e3f7ecf36bd4b97410607629c2db82320f)
    (cherry picked from commit bff3793d0aeca903518e23a2d001f5914a2ae920)

--- mkdirs.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p ./data/mongodb
-mkdir -p ./data/conf
-mkdir -p ./data/backup
-mkdir -p ./data/logs/mongodb
-mkdir -p ./data/mongodb_tests
-mkdir -p ./data/logs/mongodb_tests
