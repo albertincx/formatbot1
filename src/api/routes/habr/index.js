45f2c2f041b99c552ebaf790b48e37a61539d750
al <albertincx@gmail.com>
2019-08-21 23:18:33 +0300

    new short link rule
    (cherry picked from commit b076677e3b8bfb95fd1d440a143b64fd8600ad25)

--- src/api/routes/habr/index.js
+++ src/api/routes/habr/index.js
@@ -15,24 +15,25 @@ const sites = require('./sites');
 
 module.exports = (bot, botHelper) => {
   bot.on(
-    ['/start', '/help'],
-    msg => bot.sendMessage(msg.from.id, messages.start(), keyboards.start(bot))
-      .then(() => botHelper.sendAdmin(JSON.stringify(msg.from))),
+      ['/start', '/help'],
+      msg => bot.sendMessage(msg.from.id, messages.start(),
+          keyboards.start(bot)).
+          then(() => botHelper.sendAdmin(JSON.stringify(msg.from))),
   );
 
   // Hide keyboard
   bot.on('/hide', msg => bot.sendMessage(
-    msg.from.id,
-    'Type /help to show.',
-    { replyMarkup: 'hide' },
+      msg.from.id,
+      'Type /help to show.',
+      { replyMarkup: 'hide' },
   ));
 
   // Hide keyboard
-  bot.on('/addshort', msg => bot.sendMessage(msg.from.id, messages.sendMe())
-    .then(() => {
-      addShrtLnkStatus[msg.from.id] = 1;
-      return botHelper.sendAdmin(`link: ${msg.text}`);
-    }));
+  bot.on('/addshort',
+      msg => bot.sendMessage(msg.from.id, messages.sendMe()).then(() => {
+        addShrtLnkStatus[msg.from.id] = 1;
+        return botHelper.sendAdmin(`link: ${msg.text}`);
+      }));
 
   bot.on('*', async (msg) => {
     let txt = msg.text;
@@ -46,11 +47,10 @@ module.exports = (bot, botHelper) => {
       if (addShrtLnkStatus[msg.from.id] === 1) {
         const mess = isLinked ? '' : 'Canceled, link not found';
 
-        return bot.sendMessage(msg.from.id, mess || messages.thx())
-          .then(() => {
-            delete addShrtLnkStatus[msg.from.id];
-            return botHelper.sendAdmin(`link: ${txt}`);
-          });
+        return bot.sendMessage(msg.from.id, mess || messages.thx()).then(() => {
+          delete addShrtLnkStatus[msg.from.id];
+          return botHelper.sendAdmin(`link: ${txt}`);
+        });
       }
       const group = process.env.TGGROUP;
 
@@ -67,16 +67,14 @@ module.exports = (bot, botHelper) => {
           return newRequest.on('response', (response) => {
             newRequest.abort();
             return botHelper.sendToUser(
-              `${response.request.uri.href} `,
-              group,
-              false,
-            )
-              .then(message => bot.forwardMessage(
+                `${response.request.uri.href} `,
+                group,
+                false,
+            ).then(message => bot.forwardMessage(
                 msg.chat.id,
                 group,
                 message.message_id,
-              ))
-              .catch(error => console.log(error));
+            )).catch(error => console.log(error));
           });
         });
         if (isLinked) {
@@ -87,5 +85,14 @@ module.exports = (bot, botHelper) => {
       }
     }
     return true;
+  });
+
+  bot.on('/sendclient', msg => {
+    if (!botHelper.isAdmin(msg.chat.id)) {
+      return;
+    }
+    const msgText = msg.text.replace('/sendclient ', '');
+    const [, userId, text] = msgText.match(/([0-9]+)\s(.*?)$/);
+    botHelper.botMes(userId, text);
   });
 };
