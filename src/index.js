1d434f17f615667b99249a5ff3c3241750551d48
al <albertincx@gmail.com>
2019-08-25 00:35:32 +0300

    might with mmmmercury parser

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ const express = require('express');
 
 const app = express();
 const botroute = require('./api/routes/botroute');
+const mercury = require('./api/routes/mercury');
 const bot = require('./config/bot');
 
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.get('/', (req, res) => res.json({ code: 200 }));
+app.use('/mercury', mercury);
 app.use('/bot', botroute(bot));
 app.listen(port, () => console.info(`server started on port ${port}`));
 
