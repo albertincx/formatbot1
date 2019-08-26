f8454c2e75f5c1dd67055be82196bc8aab306ad8
al <albertincx@gmail.com>
2019-08-26 22:51:02 +0300

    update log
    (cherry picked from commit b631463f04bb6196b31e76cc9ffba2a4315f3856)

--- src/api/routes/botroute.js
+++ src/api/routes/botroute.js
@@ -1,13 +1,22 @@
 const express = require('express');
+
 const BotHelper = require('../utils/bot');
+const ha = require('./habr');
+
 const router = express.Router();
 const AL_ID = process.env.TGADMIN;
+let startCnt = 0;
 module.exports = (bot) => {
   const botHelper = new BotHelper(bot);
-  const ha = require('./habr');
   ha(bot, botHelper);
   bot.on('/srv', msg => botHelper.sendAdmin(`link: ${JSON.stringify(msg)}`));
   bot.start();
-  botHelper.botMes(AL_ID, 'started' + `👍🏻`);
+  if ((startCnt % 10) === 0 || process.env.DEV) {
+    botHelper.botMes(AL_ID, `started ${startCnt} times ` + `👍🏻`);
+  }
+  startCnt += 1;
+  if (startCnt >= 500) {
+    startCnt = 0;
+  }
   return router;
 };
