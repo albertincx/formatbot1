1ec626c7176b624e50f281f1382d8f09fd56f186
al <albertincx@gmail.com>
2019-10-13 14:30:53 +0300

    update
    (cherry picked from commit 4dddc9a902150b187bc897647847a14c9c0b084a)
    (cherry picked from commit 60fc62609c963a2990cbdf1aff4f9b38e4e3233b)

--- src/api/controllers/mercury.controller.js
+++ src/api/controllers/mercury.controller.js
@@ -2,12 +2,19 @@
 const Mercury = require('@postlight/mercury-parser');
 
 exports.get = async (req, res, next) => {
-  const { url } = req.query;
+  const { url, json } = req.query;
   if (!url) {
     return res.json({ msg: 'no url' });
   }
   try {
-    Mercury.parse(url).then(result => res.json(result));
+    return Mercury.parse(url).then(result => {
+      if (json) {
+        return res.json(result);
+      }
+      // console.log(result);
+      return res.send(result.content);
+
+    });
     /*then(result => fs.writeFileSync('./app/site.json',
         JSON.stringify(result), 'utf8'));*/
     // then(() => console.log(`${execSync('python3 app/pip.py')}`));
--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 Promise = require('bluebird'); // eslint-disable-line no-global-assign
 const cors = require('cors');
 const express = require('express');
-
+const vars = require('./config/vars');
 const app = express();
 const botroute = require('./api/routes/botroute');
 const mercury = require('./api/routes/mercury');
-const bot = require('./config/bot');
 
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.get('/', (req, res) => res.json({ code: 200 }));
 app.use('/mercury', mercury);
-app.use('/bot', botroute(bot));
+
+if (!process.env.NOBOT) {
+  // console.log('BOT START');
+  const bot = require('./config/bot');
+  app.use('/bot', botroute(bot));
+}
+
 app.listen(port, () => console.info(`server started on port ${port}`));
 
 module.exports = app;
