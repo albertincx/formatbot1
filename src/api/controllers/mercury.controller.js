5e9ff96b4a8baadbab7104f0397edc08e64ead99
al <albertincx@gmail.com>
2019-11-04 21:34:20 +0300

    fix imgs
    (cherry picked from commit 2508bf4a6ada786109fe6d788a45a24a1309955c)

--- src/api/controllers/mercury.controller.js
+++ src/api/controllers/mercury.controller.js
@@ -1,6 +1,12 @@
+const fs = require('fs');
 const Mercury = require('@postlight/mercury-parser');
 const sanitizeHtml = require('sanitize-html');
 const sanitizeHtmlForce = require('../utils/sanitize');
+
+function imgs(content) {
+  const urlRegex = /<img [^>]+\/?>/g;
+  return content.match(urlRegex);
+}
 
 const make = async (url, isJson = false, san = false) => {
   const result = await Mercury.parse(url);
@@ -8,6 +14,13 @@ const make = async (url, isJson = false, san = false) => {
     return result;
   }
   let { title, content } = result;
+  if (process.env.DEV) {
+    fs.writeFileSync('.conf/config4.html', content);
+  }
+  const imgs1 = imgs(content);
+  for (let img of imgs1) {
+    content = content.replace(img, '##@#IMG#@##');
+  }
   if (content && (content.length > 65000 || san)) {
     content = sanitizeHtml(content);
   } else {
@@ -15,6 +28,9 @@ const make = async (url, isJson = false, san = false) => {
   }
   if (content) {
     content = sanitizeHtmlForce(content);
+  }
+  for (let img of imgs1) {
+    content = content.replace('##@#IMG#@##', img);
   }
   return {
     title,
