ff021b3b2856bfdcbddccca6dcc386d895c7308e
al <albertincx@gmail.com>
2019-11-08 13:52:51 +0300

    refactoring images

--- src/api/utils/fixImages.js
+++ src/api/utils/fixImages.js
@@ -1,21 +1,43 @@
-const sizesRegex = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#/%?=~_|!:,.;]*[-A-Z0-9+&@#/%=~_|]) [0-9]+[a-zA-Z]{1,4}/gi;
+const setRegex = /srcset="/;
 const tmpReplacer = '##@#IMG#@##';
-
-const replaceImages = (content, imgs) => {
-  for (let img of imgs) {
-    content = content.replace(img, tmpReplacer);
-  }
-};
 
 const findImages = (content) => {
   const urlRegex = /<img [^>]+\/?>/g;
   return content.match(urlRegex) || [];
 };
 
+const replaceImages = (content, imgs) => {
+  for (let img of imgs) {
+    content = content.replace(img, tmpReplacer);
+  }
+  return content;
+};
+
 const restoreImages = (content, imgs) => {
   for (let img of imgs) {
+    const srcSet = img.match(setRegex);
+    if (srcSet && srcSet.length) {
+      try {
+        const srcsetAttr = img.split(setRegex)[1].replace(/"\>/, '');
+        const arr = srcsetAttr.split(',');
+        let mid = arr[Math.round((arr.length - 1) / 2)];
+        if (mid) {
+          mid = mid.trim()
+            .replace(/\s(.*?)$/, '');
+          const src = img.match(/src="(.*?)"/);
+          if (src) {
+            img = img.replace(src[0], `src="${mid}"`);
+            img = img.replace(srcsetAttr, '');
+            img = img.replace(/srcset=""\s?/, '');
+          }
+        }
+      } catch (e) {
+        // console.log(e);
+      }
+    }
     content = content.replace(tmpReplacer, img);
   }
+  return content;
 };
 
 module.exports.restoreImages = restoreImages;
