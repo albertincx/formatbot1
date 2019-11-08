3a627c4f6293e5f101a275b93f1d9e2061bbdb6c
al <albertincx@gmail.com>
2019-11-09 02:36:05 +0300

    update mercury, extract/insert youtube
    (cherry picked from commit 5a57e039e1d8a8c28c7b6d496bb2c67327dc4eec)
    (cherry picked from commit a4aca8b2331e6d87e9a70cbfffcc7ce2b7959b3c)

--- src/api/controllers/mercury.controller.js
+++ src/api/controllers/mercury.controller.js
@@ -7,6 +7,20 @@ const imgFixer = require('../utils/fixImages');
 const parse = async (userUrl, isJson = false, san = false) => {
   let result = '';
   try {
+    const matches = userUrl.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
+    const domain = matches && matches[1];
+    if (domain) {
+      var customExtractor = {
+        domain,
+        extend: {
+          iframe: {
+            selectors: [['iframe[src*=youtube]', 'src']],
+            allowMultiple: true,
+          },
+        },
+      };
+      Mercury.addExtractor(customExtractor);
+    }
     result = await Mercury.parse(userUrl);
   } catch (e) {
     return {
@@ -17,14 +31,12 @@ const parse = async (userUrl, isJson = false, san = false) => {
   if (isJson) {
     return result;
   }
-  let { title, content, url: source } = result;
+  let { title, content, url: source, iframe } = result;
   if (content) {
     logger(content, 'mercury.html');
     const imgs = imgFixer.findImages(content);
     content = imgFixer.replaceImages(content, imgs);
-
     logger(`before san ${content.length}`);
-
     if ((content.length > 65000 || san)) {
       content = sanitizeHtml(content);
     } else {
@@ -32,15 +44,17 @@ const parse = async (userUrl, isJson = false, san = false) => {
     }
     content = sanitizeHtmlForce(content);
     content = imgFixer.restoreImages(content, imgs);
+    content = imgFixer.insertYoutube(content, iframe);
     logger(content, 'tg_content.html');
     logger(`after san ${content.length}`);
   }
-
-  return {
-    title,
+  const res = {
+    title: title || userUrl,
     content,
     source,
   };
+
+  return res;
 };
 
 exports.get = async (req, res, next) => {
--- src/api/utils/fixImages.js
+++ src/api/utils/fixImages.js
@@ -1,45 +1,64 @@
 const setRegex = /srcset="/;
-const tmpReplacer = '##@#IMG#@##';
+const imgReplacer = '##@#IMG#@##';
+
+const findSrcSet = (img) => {
+  const srcSet = img.match(setRegex);
+  if (srcSet && srcSet.length) {
+    try {
+      const srcsetAttr = img.split(setRegex)[1].replace(/"\>/, '');
+      const arr = srcsetAttr.split(',');
+      let mid = arr[Math.round((arr.length - 1) / 2)];
+      if (mid) {
+        mid = mid.trim()
+          .replace(/\s(.*?)$/, '');
+        const src = img.match(/src="(.*?)"/);
+        if (src) {
+          img = img.replace(src[0], `src="${mid}"`);
+          img = img.replace(srcsetAttr, '');
+          img = img.replace(/srcset=""\s?/, '');
+        }
+      }
+    } catch (e) {
+      // console.log(e);
+    }
+  }
+  return img;
+};
 
 const findImages = (content) => {
   const urlRegex = /<img [^>]+\/?>/g;
   return content.match(urlRegex) || [];
 };
 
-const replaceImages = (content, imgs) => {
-  for (let img of imgs) {
-    content = content.replace(img, tmpReplacer);
-  }
-  return content;
-};
-
-const restoreImages = (content, imgs) => {
-  for (let img of imgs) {
-    const srcSet = img.match(setRegex);
-    if (srcSet && srcSet.length) {
-      try {
-        const srcsetAttr = img.split(setRegex)[1].replace(/"\>/, '');
-        const arr = srcsetAttr.split(',');
-        let mid = arr[Math.round((arr.length - 1) / 2)];
-        if (mid) {
-          mid = mid.trim()
-            .replace(/\s(.*?)$/, '');
-          const src = img.match(/src="(.*?)"/);
-          if (src) {
-            img = img.replace(src[0], `src="${mid}"`);
-            img = img.replace(srcsetAttr, '');
-            img = img.replace(/srcset=""\s?/, '');
-          }
-        }
-      } catch (e) {
-        // console.log(e);
-      }
+const insertYoutube = (content, links = []) => {
+  for (let link of links) {
+    let youid = link.match(/embed\/(.*?)(\?|$)/);
+    if (youid && youid[1]) {
+      const src = `/embed/youtube?url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D${youid[1]}`;
+      content = `<figure><iframe src="${src}"></iframe></figure>${content}`;
     }
-    content = content.replace(tmpReplacer, img);
   }
   return content;
 };
 
-module.exports.restoreImages = restoreImages;
-module.exports.replaceImages = replaceImages;
+const replaceTags = (content, imgs, replaceWith) => {
+  for (let img of imgs) {
+    content = content.replace(img, replaceWith);
+  }
+  return content;
+};
+
+const restoreTags = (content, imgs, replaceFrom) => {
+  for (let img of imgs) {
+    if (replaceFrom === imgReplacer) {
+      img = findSrcSet(img);
+    }
+    content = content.replace(imgReplacer, img);
+  }
+  return content;
+};
+
 module.exports.findImages = findImages;
+module.exports.replaceImages = (content, imgs) => replaceTags(content, imgs, imgReplacer);
+module.exports.restoreImages = (content, imgs) => restoreTags(content, imgs, imgReplacer);
+module.exports.insertYoutube = insertYoutube;
