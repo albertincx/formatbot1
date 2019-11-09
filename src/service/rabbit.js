e5edee16730b9e685db18e93cb23bfa87c26ec68
al <albertincx@gmail.com>
2019-11-09 07:23:50 +0300

    try,catch
    (cherry picked from commit ed2f93f352146e45063cbc9288200ae97492bd31)

--- src/service/rabbit.js
+++ src/service/rabbit.js
@@ -1,7 +1,12 @@
 const amqp = require('amqplib');
 module.exports.start = async () => {
-  const connection = await amqp.connect(process.env.MESSAGE_QUEUE);
-  const channel = await connection.createChannel();
-  await channel.assertQueue('tasks', { durable: true });
+  let channel;
+  try {
+    const connection = await amqp.connect(process.env.MESSAGE_QUEUE);
+    channel = await connection.createChannel();
+    await channel.assertQueue('tasks', { durable: true });
+  } catch (e) {
+    console.log(e);
+  }
   return channel;
 };
