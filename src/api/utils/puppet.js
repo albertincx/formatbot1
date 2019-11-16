ed4cb90fce6658cb8c77e301872ada3e155c9d84
al <albertincx@gmail.com>
2019-11-17 02:11:10 +0300

    ref
    (cherry picked from commit 3cb5500f5bafacb6a214b5d9e7c53efcafa62965)

---
+++ src/api/utils/puppet.js
@@ -0,0 +1,46 @@
+const puppeteer = require('puppeteer');
+const logger = require('../utils/logger');
+
+const getBrowser = async () => {
+  const args = [
+    '--disable-web-security',
+    '--ignore-certificate-errors',
+    '--no-sandbox',
+    '--ignore-autoplay-restrictions',
+    '--disable-dev-shm-usage',
+    '--disable-background-timer-throttling',
+    '--lang=en',
+    '--incognito',
+  ];
+  return puppeteer.launch({
+    headless: true,
+    args,
+  })
+    .then((browser) => {
+      const browserWSEndpoint = browser.wsEndpoint();
+      browser.disconnect();
+      return browserWSEndpoint;
+    });
+};
+const puppet = async (url, ws) => {
+  try {
+    // console.log('Worker ' + process.pid + ' received message from master.', msg);
+    // Use the endpoint to reestablish a connection
+    const browser = await puppeteer.connect({ browserWSEndpoint: ws });
+    const page = await browser.newPage();
+    const status = await page.goto(url);
+    if (!status.ok) {
+      throw new Error('cannot open google.com');
+    }
+    const content = await page.content();
+    page.close();
+    // Close Chromium
+    await browser.close();
+    return content;
+  } catch (e) {
+    logger(e);
+  }
+  return '';
+};
+module.exports = puppet;
+module.exports.getBrowser = getBrowser;
