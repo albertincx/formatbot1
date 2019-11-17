59206a988e8249d0e37c7515b15bd157dc8eebf6
al <albertincx@gmail.com>
2019-11-17 12:58:39 +0300

    ref

--- src/test/testPuppet.js
+++ src/test/testPuppet.js
@@ -156,7 +156,7 @@ test('bot.TelegraphLinkAsync', async (t) => {
   return bot.sendMessage(USER, 'wait')
     .then(async (re) => {
       let browserWs = await puppet.getBrowser();
-      let url = 'https://theinitium.com/article/20191113-hongkong-cuhk-protest-police/';
+      let url = process.env.TEST_LINK_ASYNC;
       const ivMaker = require('../api/utils/ivMaker');
       const { iv, source, isLong } = await ivMaker.makeIvLink(url, browserWs);
       const chatId = USER;
