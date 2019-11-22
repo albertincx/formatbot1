a67a7e70f881d43e5c4d2f13ff6d89e06707d74c
al <albertincx@gmail.com>
2019-11-22 22:01:41 +0300

    ref

--- src/api/routes/mercury/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const mercury = require('../../controllers/mercury.controller');
-
-const router = express.Router();
-
-router.route('/').get(mercury.get);
-module.exports = router;
