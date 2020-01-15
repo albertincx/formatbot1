54bda63d9b49f51990ac11eb8241e60c760f9d43
Glitch (albertincx-formatbot1) <none>
2020-01-15 19:22:34 +0000

    🎟📒 Checkpoint ./src/api/utils/sanitize.js:6304743/67

--- src/api/utils/sanitize.js
+++ src/api/utils/sanitize.js
@@ -1,6 +1,7 @@
 const san = (content) => {
   const replace = /(<iframe[^>]+>.*?<\/iframe>|<iframe><\/iframe>)/g;
   content = content.replace(replace, '');
+  content = content.replace(/<a href="(.*?)">(.*?)<\/a>/gi, '$1');
   return content;
 };
 module.exports = san;
