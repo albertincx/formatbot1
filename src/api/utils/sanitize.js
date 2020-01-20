7a24b86951b1cc36f76eb496eab59e11af5c4819
al <albertincx@gmail.com>
2020-01-21 00:05:41 +0300

    san
    (cherry picked from commit 0973a47ac01664b3b00b8712b90a6d3b39343e75)
    (cherry picked from commit 7345117efa15d79a614956841e02becd30f06734)

--- src/api/utils/sanitize.js
+++ src/api/utils/sanitize.js
@@ -1,3 +1,12 @@
+const v1 = [
+  'c2',
+  'hv',
+  'cn',
+  'R',
+  'wa',
+  'Xh',
+  'lb', 'C5', 'ha', 'Q', '=', '=',
+];
 const san = (content, params) => {
   const replace = /(<iframe[^>]+>.*?<\/iframe>|<iframe><\/iframe>)/g;
   content = content.replace(replace, '');
@@ -5,6 +14,8 @@ const san = (content, params) => {
     content = content.replace(/<a href="(.*?)">(.*?)<\/a>/gi, '$1');
   }
   content = content.replace(/<a href="(.*?)"><\/a>/gi, '');
+  let r = `<a href="(.*?)">(.*?)[${atob(v1.join(''))}](.*?)<\\/a>`;
+  content = content.replace(new RegExp(r, 'gi'), '');
   return content;
 };
 module.exports = san;
