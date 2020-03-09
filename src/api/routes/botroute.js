33e077ac9833888372a4bc6b143a65e92ab73c66
Glitch (albertincx-formatbot1) <none>
2020-03-09 04:46:46 +0000

    👋☔️ Checkpoint ./src/api/routes/botroute.js:6304743/44

--- src/api/routes/botroute.js
+++ src/api/routes/botroute.js
@@ -48,6 +48,7 @@ module.exports = (bot, conn) => {
   });
 
   bot.command('srv', ({ message }) => {
+    console.log(JSON.stringify(message));
     if (botHelper.isAdmin(message.chat.id)) {
       botHelper.sendAdmin(`srv: ${JSON.stringify(message)}`);
     }
