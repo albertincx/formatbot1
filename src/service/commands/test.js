c7be7746021d301b4086f7968e5a5238568ea315
albert <albertincx@gmail.com>
2020-03-22 10:52:27 +0300

    ref, cron
    (cherry picked from commit 38aab1cfb62321ce1a61a30566bbad57c04dc23d)

---
+++ src/service/commands/test.js
@@ -0,0 +1,14 @@
+const fetch = require('node-fetch');
+
+async function run(params, botHelper) {
+  try {
+    let url = process.env.TEST_API;
+    if (url) return;
+    await fetch(url);
+    await botHelper.sendAdmin('cron test check url');
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+module.exports = { run };
