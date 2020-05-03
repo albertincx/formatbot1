837584277c85b1871651649e56d8d0891eba0ade
albert <albertincx@gmail.com>
2020-05-03 22:10:19 +0300

    remove proxy
    (cherry picked from commit b2dd850bb6a1110f601aa2af70575e8a12a5d4ca)
    (cherry picked from commit 34558ffa4e168d665050e590e11ab79b8ea5a980)

--- src/config/bot.js
+++ src/config/bot.js
@@ -1,12 +1,6 @@
 const Telegraf = require('telegraf');
 
 const opts = {};
-const SocksAgent = require('socks5-https-client/lib/Agent');
-const socksAgent = new SocksAgent({
-  socksHost: process.env.SOCKS_HOST,
-  socksPort: process.env.SOCKS_PORT,
-});
-opts.telegram = { agent: socksAgent };
 
 let bot = false;
 let botToken = process.env.TBTKN;
