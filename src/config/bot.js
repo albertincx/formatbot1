be8fe11f8103265dc37486b18cdfc0d78fdd947c
albert <albertincx@gmail.com>
2020-08-27 00:56:55 +0300

    channel messages
    (cherry picked from commit 0f975fe34908b518e874c02a5ad84ffa2fa9f294)
    (cherry picked from commit 4780166b238c7fc4f9924b8966ebd8ea4b83ccd9)

--- src/config/bot.js
+++ src/config/bot.js
@@ -1,9 +1,9 @@
 const Telegraf = require('telegraf');
 
-const opts = {};
+const opts = { channelMode: true };
 
 let bot = false;
-let botToken = process.env.TBTKN;
+const botToken = process.env.TBTKN;
 if (botToken) {
   bot = new Telegraf(botToken, opts);
 }
