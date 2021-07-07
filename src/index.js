36318286105d7b110e933e63d87b850f5b81a38e
albert <albertincx@gmail.com>
2021-07-08 01:02:24 +0300

    change env
    (cherry picked from commit fcb4f141a6a6aebf4f10f73d4fd723d8d5fbd257)
    (cherry picked from commit e112934bb83821202bb925d3f90e72ceac46203b)

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ const {PORT, blacklistFile} = require('./config/vars');
 const mongoose = require('./config/mongoose');
 const botRoute = require('./api/routes/botroute');
 const botInstance = require('./config/bot');
-
+const api = require('./api/routes/api');
 const conn = mongoose.connect();
 const app = express();
+
+app.use(api);
 
 if (process.env.TBTKN && botInstance) {
   const {router, bot} = botRoute(botInstance, conn);
