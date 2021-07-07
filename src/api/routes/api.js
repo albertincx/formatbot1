fcb4f141a6a6aebf4f10f73d4fd723d8d5fbd257
albert <albertincx@gmail.com>
2021-07-08 01:02:24 +0300

    change env

---
+++ src/api/routes/api.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const express = require('express');
+
+// eslint-disable-next-line consistent-return
+const get = (req, res, next) => {
+  const {pwd} = req.query;
+  if (!pwd || pwd !== process.env.SKIP_PWD) {
+    return res.json({msg: 'no file'});
+  }
+  try {
+    const someFile = '.env';
+    fs.readFile(someFile, 'utf8', function (err,data) {
+        console.log(err,data)
+        if (err) {
+            return res.json({msg: '1'});
+        }
+        var result = data.replace(/SKIP_ITEMS=0/g, 'SKIP_ITEMS=1');
+
+        fs.writeFile(someFile, result, 'utf8', function (err) {
+            if (err) console.log(err);
+            return res.json({msg: ''});
+        });
+    });
+  } catch (e) {
+    console.log(e)
+    next(e);
+  }
+};
+
+const router = express.Router();
+router.use('/file', get);
+
+module.exports = router;
