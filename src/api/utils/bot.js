75de81b06f9466e1bf17484371213f8bd5014537
albertincx <albertincx@gmail.com>
2022-04-24 21:44:28 +0200

    show error
    (cherry picked from commit 6c4c24f5bb68088c2fb2956ae32d94215ed82952)
    (cherry picked from commit d645001c83cbcad63f6c9ecb4030b9f8cd5bc833)

--- src/api/utils/bot.js
+++ src/api/utils/bot.js
@@ -7,6 +7,8 @@ const ON = 'On';
 const PARSE_MODE_MARK = 'Markdown';
 
 const INLINE_TITLE = 'InstantView created. Click me to send';
+const BANNED_ERROR = 'USER_BANNED_IN_CHANNEL';
+const RIGHTS_ERROR = 'need administrator rights in the channel chat';
 
 class BotHelper {
   constructor(bot) {
@@ -180,6 +182,9 @@ class BotHelper {
     if (typeof e === 'object' && !global.isDevEnabled) {
       if (e.response && typeof e.response === 'object') {
         e = e.response.description || 'unknown error';
+        if (e.match(BANNED_ERROR) || e.match(RIGHTS_ERROR)) {
+          return;
+        }
       }
     } else {
       e = `error: ${JSON.stringify(e)} ${e.toString()} ${text}`;
