680c2462da15d0e55f681d8b8a43fc977c90bfc8
albertincx <albertincx@gmail.com>
2024-04-17 13:36:06 +0700

    some refactoring back

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -1,5 +1,22 @@
 const mongoose = require('mongoose');
-const {NO_DB} = require('./vars');
+const {NO_DB, MONGO_URI} = require('./vars');
+
+exports.connect = uri => {
+  const dbUri = uri || MONGO_URI;
+
+  if (!dbUri || NO_DB) {
+    return false;
+  }
+
+  mongoose.connect(dbUri, {
+    keepAlive: true,
+    connectTimeoutMS: 30000,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  return mongoose.connection;
+};
 
 const createConnection = (uri) => {
   if (!uri || NO_DB) {
--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@ require('trace-unhandled/register');
 
 const botRoute = require('./api/routes/botroute');
 const botInstance = require('./config/bot');
-const {MONGO_URI} = require('./config/vars');
 // const botInstance = require('./config/botTest');
-const conn = require('./config/mongoose').createConnection(MONGO_URI);
+const conn = require('./config/mongoose').connect();
 
 if (botInstance) {
   botRoute(botInstance, conn);
