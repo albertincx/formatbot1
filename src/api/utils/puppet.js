93ed4b23a9527963a8dfe7a427b25e9b0025aabe
albertincx <albertincx@gmail.com>
2024-05-30 16:25:09 +0700

    puppet

--- src/api/utils/puppet.js
+++ src/api/utils/puppet.js
@@ -4,70 +4,71 @@ const {logger} = require('./logger');
 const {HEADLESS} = require('../../config/vars');
 
 const getBrowser = async () => {
-  const args = [
-    '--disable-web-security',
-    '--ignore-certificate-errors',
-    '--no-sandbox',
-    '--ignore-autoplay-restrictions',
-    '--disable-dev-shm-usage',
-    '--disable-background-timer-throttling',
-    '--lang=en',
-    '--incognito',
-  ];
-  const opts = {args};
-  if (HEADLESS !== '1') {
-    opts.headless = 'new';
-  }
-  return puppeteer.launch(opts).then(browser => {
-    const browserWSEndpoint = browser.wsEndpoint();
-    browser.disconnect();
-    return browserWSEndpoint;
-  });
+    const args = [
+        '--disable-web-security',
+        '--ignore-certificate-errors',
+        '--no-sandbox',
+        '--ignore-autoplay-restrictions',
+        '--disable-dev-shm-usage',
+        '--disable-background-timer-throttling',
+        '--lang=en',
+        '--incognito',
+        '--disabled-setupid-sandbox',
+    ];
+    const opts = {args};
+    if (HEADLESS !== '1') {
+        opts.headless = 'new';
+    }
+    return puppeteer.launch(opts).then(browser => {
+        const browserWSEndpoint = browser.wsEndpoint();
+        browser.disconnect();
+        return browserWSEndpoint;
+    });
 };
 
 const puppet = async (url, params) => {
-  const {browserWs: ws, scroll} = params;
-  if (!ws) {
-    return Promise.resolve('');
-  }
-  logger('puppet start');
-  logger(new Date());
-  try {
-    logger(url);
-    const browser = await puppeteer.connect({browserWSEndpoint: ws});
-    const page = await browser.newPage();
-    const status = await page
-      .goto(url, {waitUntil: 'load', timeout: 30000})
-      .catch(() => page.close().then(() => page.content()));
-    if (!status.ok) {
-      logger('cannot open google.com');
-    } else {
-      logger('wait');
-      const scCnt = scroll ? 6 : 3;
-      logger(scroll);
-      await timeout(5);
-      for (let i = 0; i < scCnt; i += 1) {
-        // eslint-disable-next-line no-await-in-loop,no-loop-func
-        await page.evaluate(sc => {
-          window.scrollBy(0, 200);
-          const s = document.getElementById(sc);
-          if (s) {
-            s.scrollTop += 200;
-          }
-        }, scroll);
-      }
-      await timeout(2);
-      logger('wait 2');
-      const content = await page.content();
-      await page.close();
-      logger('puppet end');
-      logger(new Date());
-      return content;
+    const {browserWs: ws, scroll} = params;
+    if (!ws) {
+        return Promise.resolve('');
     }
-  } catch (e) {
-    logger(e);
-  }
-  return '';
+    logger('puppet start');
+    logger(new Date());
+    try {
+        logger(url);
+        const browser = await puppeteer.connect({browserWSEndpoint: ws});
+        const page = await browser.newPage();
+        const status = await page
+            .goto(url, {waitUntil: 'load', timeout: 30000})
+            .catch(() => page.close().then(() => page.content()));
+        if (!status.ok) {
+            logger('cannot open google.com');
+        } else {
+            logger('wait');
+            const scCnt = scroll ? 6 : 3;
+            logger(scroll);
+            await timeout(5);
+            for (let i = 0; i < scCnt; i += 1) {
+                // eslint-disable-next-line no-await-in-loop,no-loop-func
+                await page.evaluate(sc => {
+                    window.scrollBy(0, 200);
+                    const s = document.getElementById(sc);
+                    if (s) {
+                        s.scrollTop += 200;
+                    }
+                }, scroll);
+            }
+            await timeout(2);
+            logger('wait 2');
+            const content = await page.content();
+            await page.close();
+            logger('puppet end');
+            logger(new Date());
+            return content;
+        }
+    } catch (e) {
+        logger(e);
+    }
+    return '';
 };
 module.exports = puppet;
 module.exports.getBrowser = getBrowser;
